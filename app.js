6b96d5e7ef3b60ee19591117cf6d37c0f419f11d
Kevin <kfig21@gmail.com>
2021-10-22 00:55:55 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -17,9 +18,7 @@ app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var dev_db_url =
-  "mongodb+srv://ppuser:pppassword@ppcluster.jbbh2.mongodb.net/catalog?retryWrites=true&w=majority";
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = process.env.DB_CONNECTION_STRING;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
